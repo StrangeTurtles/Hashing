ffc625a56cd8b2a3727d377077c19a6330dc012f
Robert Garcia-Allen <s199030@students.aie.edu>
2020-02-04 10:52:03 -0800

    Testing finish

--- Hashing/Main.cpp
+++ Hashing/Main.cpp
@@ -7,7 +7,19 @@ int main()
 	tHashmap<std::string, int> favoriteNumbersByName;
 	favoriteNumbersByName["Terry"] = 5;
 
+	tHashmap<std::string, int> favoriteNumbersByNameCpy(favoriteNumbersByName);
+
 	std::cout << favoriteNumbersByName["Terry"] << std::endl; // 5
+	std::cout << favoriteNumbersByNameCpy["Terry"] << std::endl; // 5
+
+	tHashmap<std::string, int> testWord;
+	testWord["Robert"] = 13;
+
+	testWord["Jon"] = 3;
+
+	std::cout << testWord["Robert"] << std::endl; // 13
+	std::cout << testWord["Jon"] << std::endl; // 3
+
 	while (true)
 	{
 
--- Hashing/tHashmap.h
+++ Hashing/tHashmap.h
@@ -8,8 +8,10 @@ class tHashmap
 
 public:
 	tHashmap();                     // constructs the hashmap with a specific size
+	tHashmap(int amount);
 	~tHashmap();                    // cleans-up any underlying data
 
+	tHashmap(const tHashmap& other);
 
 	V& operator= (const tHashmap& other);
 
@@ -24,16 +26,42 @@ tHashmap<K, V>::tHashmap()
 }
 
 template<typename K, typename V>
-tHashmap<K, V>::~tHashmap()
+inline tHashmap<K, V>::tHashmap(int amount)
 {
-	delete(data);
-	delete(dataCapacity);
+	dataCapacity = amount;
+	data = new V[dataCapacity];
 }
 
 template<typename K, typename V>
-inline V & tHashmap<K, V>::operator=(const tHashmap & other)
+tHashmap<K, V>::~tHashmap()
+{
+	dataCapacity = 0;
+	data = 0;
+}
+
+template<typename K, typename V>
+inline tHashmap<K, V>::tHashmap(const tHashmap & other)
+{
+	data = new V[other.dataCapacity];
+	dataCapacity = other.dataCapacity;
+	for (int i = 0; i < dataCapacity; i++)
+	{
+		data[i] = other.data[i];
+	}
+}
+
+//if the data capacity is the same then copy the data to the lhs
+template<typename K, typename V>
+inline V & tHashmap<K, V>::operator=(const tHashmap & rhs)
 {
 	// TODO: insert return statement here
+	if (dataCapacity == rhs.dataCapacity)
+	{
+		for (int i = 0; i < dataCapacity; i++)
+		{
+			data[i] = rhs.data[i];
+		}
+	}
 }
 
 template<typename K, typename V>
